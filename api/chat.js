864600885b7b5ad05e8a35a6021b7a1ce1606f56
rekuskz-gif <rekuskz@gmail.com>
2026-04-05 23:53:40 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -72,7 +72,7 @@ module.exports = async (req, res) => {
         "content-type": "application/json"
       },
       body: JSON.stringify({
-        model: "claude-haiku-4-5-20251001",
+        model: "claude-haiku-4-5",
         max_tokens: 300,
         system: systemPrompt,
         messages: messages
